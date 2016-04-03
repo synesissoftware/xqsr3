8484f62d3248782b9d1a35b0719600c92f1d3d91
Matt <matthew@synesis.com.au>
2016-04-04 07:49:54 +1000

    + added Xqsr3::IO.writelines()

---
+++ lib/xqsr3/io/writelines.rb
@@ -0,0 +1,146 @@
+# ######################################################################### #
+# File:         xqsr3/io/writelines.rb
+#
+# Purpose:      Adds a writelines() method to the IO module
+#
+# Created:      13th April 2007
+# Updated:      4th April 2016
+#
+# Author:       Matthew Wilson
+#
+# Copyright (c) 2007-2016, Matthew Wilson and Synesis Software
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# ######################################################################### #
+
+# ##########################################################
+# Xqsrt3::IO
+
+module Xqsr3
+module IO
+
+	private
+	module Constants_
+
+		NUMBER_OF_LINES_TO_EXAMINE	=	20
+
+	end # module Constants_
+
+	private
+
+	def self.write_to_target_ target, contents, line_separator, column_separator
+$stderr.puts "#{self.class}.write_to_target_(target(#{target.class})='#{target}', contents(#{contents.class})='#{contents}', line_separator(#{line_separator.class})='#{line_separator}', column_separator=(#{column_separator.class})='#{column_separator}')" if $DEBUG
+		if contents.instance_of? ::Hash
+
+			contents.each do |k, v|
+
+				target << k << column_separator << v << line_separator
+			end
+		else
+
+			contents.each do |element|
+
+				target << element << line_separator
+			end
+		end
+
+		contents.size
+	end
+
+	def self.deduce_line_separator_ contents
+
+		if contents.instance_of? ::Hash
+
+			contents.each_with_index do |k, v, index|
+
+				if Constants_::NUMBER_OF_LINES_TO_EXAMINE == index
+
+					break
+				else
+
+					return '' if v.to_s.include? "\n"
+					return '' if k.to_s.include? "\n"
+				end
+			end
+		else
+
+			contents.each_with_index do |element, index|
+
+				if Constants_::NUMBER_OF_LINES_TO_EXAMINE == index
+
+					break
+				else
+
+					return '' if element.to_s.include? "\n"
+				end
+			end
+		end
+
+		"\n"
+	end
+
+	public
+
+	#
+	# 
+	def self.writelines(target, contents, line_separator = nil, column_separator = nil)
+
+		# process parameters
+
+		if contents.instance_of? String
+
+			if contents.include? "\n"
+
+				contents = contents.split(/\r?\n/, -1)
+			else
+
+				contents = [ contents ]
+			end
+		end
+
+		column_separator	||=	''
+		line_separator		||=	self.deduce_line_separator_ contents
+
+		if not contents.kind_of? ::Enumerable and not contents.instance_of? ::Hash
+
+			raise ArgumentError, "writelines() must be passed a #{::String}, or a #{::Hash}, or an #{::Enumerable} (or derived)"
+		end
+
+		if ::String == target
+
+			File.open(path, "w") do |io|
+
+				self.write_to_target_ io, contents, line_separator, column_separator
+			end
+		else
+
+			self.write_to_target_ target, contents, line_separator, column_separator
+		end
+	end # writelines
+end # module IO
+end # module Xqsr3
+
+# ############################## end of file ############################# #
+
---
+++ test/unit/io/tc_writelines.rb
@@ -0,0 +1,112 @@
+#! /usr/bin/ruby
+
+$:.unshift File.join(File.dirname(__FILE__), '../../lib')
+
+require 'xqsr3/io/writelines'
+
+require 'test/unit'
+
+require 'stringio'
+
+include ::Xqsr3::IO
+
+class Test_IO_writelines < Test::Unit::TestCase
+
+	def test_single_string
+
+		input = 'abc'
+
+		s = StringIO.new '', 'a'
+
+		r = ::Xqsr3::IO.writelines s, input
+
+		assert_equal 1, r
+		assert_equal "abc\n", s.string
+	end
+
+	def test_single_string_in_array
+
+		input = [ 'abc' ]
+
+		s = StringIO.new '', 'a'
+
+		r = ::Xqsr3::IO.writelines s, input
+
+		assert_equal 1, r
+		assert_equal "abc\n", s.string
+	end
+
+	def test_single_string_in_hash
+
+		input = { 'abc' => '' }
+
+		s = StringIO.new '', 'a'
+
+		r = ::Xqsr3::IO.writelines s, input
+
+		assert_equal 1, r
+		assert_equal "abc\n", s.string
+	end
+
+	def test_two_strings_in_array
+
+		input = [ 'abc', 'def' ]
+
+		s = StringIO.new '', 'a'
+
+		r = ::Xqsr3::IO.writelines s, input
+
+		assert_equal 2, r
+		assert_equal "abc\ndef\n", s.string
+	end
+
+	def test_two_strings_in_array_with_suppressed_eol
+
+		input = [ 'abc', 'def' ]
+
+		s = StringIO.new '', 'a'
+
+		r = ::Xqsr3::IO.writelines s, input, ''
+
+		assert_equal 2, r
+		assert_equal "abcdef", s.string
+	end
+
+	def test_two_strings_in_hash
+
+		input = { 'ab' => 'c', 'de' => 'f' }
+
+		s = StringIO.new '', 'a'
+
+		r = ::Xqsr3::IO.writelines s, input
+
+		assert_equal 2, r
+		assert_equal "abc\ndef\n", s.string
+	end
+
+	def test_ten_strings_in_array
+
+		input = (0...10).map { |i| i.to_s }
+
+		s = StringIO.new
+
+		r = ::Xqsr3::IO.writelines s, input
+
+		assert_equal 10, r
+		assert_equal "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n", s.string
+	end
+
+	def test_strings_with_cr_in_array
+
+		input = [ "abc\n", "def\n", "ghi\n" ]
+
+		s = StringIO.new
+
+		r = ::Xqsr3::IO.writelines s, input
+
+		assert_equal 3, r
+		assert_equal "abc\ndef\nghi\n", s.string
+	end
+end
+
+
