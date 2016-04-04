9dcdd48dbb1f4e3a32c84cf05fb1921e1789de43
Matt <matthew@synesis.com.au>
2016-04-04 14:06:10 +1000

    + added Enumerable.collect_with_index()

---
+++ test/unit/extensions/enumerable/tc_collect_with_index.rb
@@ -0,0 +1,36 @@
+#! /usr/bin/ruby
+
+$:.unshift File.join(File.dirname(__FILE__), '../../../../lib')
+
+require 'xqsr3/extensions/enumerable/collect_with_index'
+
+require 'test/unit'
+
+class Test_Enumerable_collect_with_index < Test::Unit::TestCase
+
+	def test_simple
+
+		ar = [ 1, -2, 3, -4, 5, -6 ]
+
+		r = ar.collect_with_index do |v, index0|
+
+			index0 * v.abs
+		end
+
+		assert_equal [ 0, 2, 6, 12, 20, 30 ], r
+	end
+
+	def test_simple_with_offset
+
+		ar = [ 1, -2, 3, -4, 5, -6 ]
+
+		r = ar.collect_with_index(1) do |v, index0|
+
+			index0 * v.abs
+		end
+
+		assert_equal [ 1, 4, 9, 16, 25, 36 ], r
+	end
+end
+
+
