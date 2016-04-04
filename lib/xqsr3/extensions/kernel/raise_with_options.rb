19ae625c07b531fe4f24abb35900430a53e9b601
Matt <matthew@synesis.com.au>
2016-04-04 13:47:02 +1000

    + copyright information, etc.

--- lib/xqsr3/extensions/kernel/raise_with_options.rb
+++ lib/xqsr3/extensions/kernel/raise_with_options.rb
@@ -2,19 +2,52 @@
 # ######################################################################## #
 # File:         lib/xqsr3/extensions/kernel/raise_with_options.rb
 #
-# Purpose:      Extends the Kernel module with the raise_with_options
-#               method
+# Purpose:      Adds a raise_with_options() method to the Kernel module
 #
 # Created:      12th February 2015
 # Updated:      3rd April 2016
 #
+# Home:         http://github.com/synesissoftware/xqsr3
+#
 # Author:       Matt Wilson
 #
-# Copyright:    Synesis Software Pty Ltd, 2015-2016
+# Copyright (c) 2015-2016, Matthew Wilson and Synesis Software
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the names of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # ######################################################################## #
 
+
 require 'xqsr3/diagnostics/exception_utilities'
+
+# ##########################################################
+# ::Kernel
 
 module Kernel
 
@@ -24,7 +57,7 @@ module Kernel
 
 		::Xqsr3::Diagnostics::ExceptionUtilities.raise_with_options *args, **(options.merge({ :called_indirectly_06d353cb_5a6c_47ca_8dbe_ff76359c7e96 => 1}))
 	end
-end
+end # module Kernel
 
 # ############################## end of file ############################# #
 
