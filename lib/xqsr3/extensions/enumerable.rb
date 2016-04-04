58585a42141b9c3f4c62066f6abcceb8248b9fed
Matt <matthew@synesis.com.au>
2016-04-04 14:08:59 +1000

    + added Enumerable.collect_with_index()

--- lib/xqsr3/extensions/enumerable.rb
+++ lib/xqsr3/extensions/enumerable.rb
@@ -1,5 +1,6 @@
 
-require 'xqsr3/extensions/enumerable/collection_with_index'
+require 'xqsr3/extensions/enumerable/collect_with_index'
+require 'xqsr3/extensions/enumerable/find_index_from'
 require 'xqsr3/extensions/enumerable/pure_unique'
 require 'xqsr3/extensions/enumerable/unique'
 
