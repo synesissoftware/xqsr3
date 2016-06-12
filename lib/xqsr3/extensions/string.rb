58ddca5b01f6ecbe2afdd82fc4c2ea85047bf142
Matt <stlsoft@gmail.com>
2016-06-13 09:16:19 +1000

    ~ adding missing inclusion for map_option_string into extensions/string.rb

--- lib/xqsr3/extensions/string.rb
+++ lib/xqsr3/extensions/string.rb
@@ -1,5 +1,6 @@
 
 require 'xqsr3/extensions/string/ends_with'
+require 'xqsr3/extensions/string/map_option_string'
 require 'xqsr3/extensions/string/starts_with'
 require 'xqsr3/extensions/string/to_symbol'
 
--- lib/xqsr3/version.rb
+++ lib/xqsr3/version.rb
@@ -5,7 +5,7 @@
 # Purpose:      Version for Xqsr3 library
 #
 # Created:      3rd April 2016
-# Updated:      10th May 2016
+# Updated:      13th May 2016
 #
 # Home:         http://github.com/synesissoftware/xqsr3
 #
@@ -50,7 +50,7 @@
 module Xqsr3
 
 	# Current version of the Xqsr3 library
-	VERSION				=	'0.8.3'
+	VERSION				=	'0.8.4'
 
 	private
 	VERSION_PARTS_		=	VERSION.split(/[.]/).collect { |n| n.to_i } # :nodoc:
