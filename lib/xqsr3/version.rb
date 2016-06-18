a435463c36486c136f472f060f3dab0b45f7e838
Matthew <matthew@synesis.com.au>
2016-06-19 09:58:46 +1000

    + added lib/xqsr3/quality

--- lib/xqsr3/version.rb
+++ lib/xqsr3/version.rb
@@ -5,7 +5,7 @@
 # Purpose:      Version for Xqsr3 library
 #
 # Created:      3rd April 2016
-# Updated:      13th May 2016
+# Updated:      19th May 2016
 #
 # Home:         http://github.com/synesissoftware/xqsr3
 #
@@ -50,7 +50,7 @@
 module Xqsr3
 
 	# Current version of the Xqsr3 library
-	VERSION				=	'0.8.5'
+	VERSION				=	'0.8.6'
 
 	private
 	VERSION_PARTS_		=	VERSION.split(/[.]/).collect { |n| n.to_i } # :nodoc:
