9714fa63bf1d9cefe7482194036fbd0565f06914
Matt <stlsoft@gmail.com>
2017-06-07 17:21:45 +1000

    + added String.to_bool extension

--- lib/xqsr3/extensions/string.rb
+++ lib/xqsr3/extensions/string.rb
@@ -3,5 +3,6 @@ require 'xqsr3/extensions/string/ends_with'
 require 'xqsr3/extensions/string/map_option_string'
 require 'xqsr3/extensions/string/quote_if'
 require 'xqsr3/extensions/string/starts_with'
+require 'xqsr3/extensions/string/to_bool'
 require 'xqsr3/extensions/string/to_symbol'
 
