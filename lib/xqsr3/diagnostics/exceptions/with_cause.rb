ba0a1adb7b4f38f07ef82170696e58b3fea0c695
Matt <stlsoft@gmail.com>
2017-12-17 09:58:52 +1100

    + added Xqsr3::Diagnostics::Exceptions::WithCase inclusion module, which enables an exception class to be able to receive a cause (inner exception) in its initialiser, which is then exposed via a +cause+ attribute (along with +chainees+, +exceptions+, +chained_backtrace+, and +chained_message+ attributes)

---
+++ lib/xqsr3/diagnostics/exceptions/with_cause.rb
@@ -0,0 +1,194 @@
+
+# ######################################################################## #
+# File:         lib/xqsr3/diagnostics/exceptions/with_cause.rb
+#
+# Purpose:      Definition of the WithCause inclusion module
+#
+# Created:      16th December 2017
+# Updated:      17th December 2017
+#
+# Home:         http://github.com/synesissoftware/xqsr3
+#
+# Author:       Matthew Wilson
+#
+# Copyright (c) 2017, Matthew Wilson and Synesis Software
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the names of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# ######################################################################## #
+
+
+# ##########################################################
+# ::Xqsr3::Diagnostics::Exceptions::WithCause
+
+=begin
+=end
+
+module Xqsr3
+module Diagnostics
+module Exceptions
+
+# This inclusion module adds to an exception class the means to chain a
+# cause (aka inner-exception), which is then exposed with the +cause+
+# attribute
+#
+# *Examples:*
+#
+# Passing an exception cause as a parameter
+#
+#
+#
+module WithCause
+
+	# Defines an initializer for an exception class that allows a cause (aka
+	# an inner exception) to be specified, either as the first or last
+	# argument or as a +:cause+ option
+	#
+	# === Signature
+	#
+	# * *Parameters:*
+	#  - 
+	#  - +option+:: 
+	#
+	# * *Options:*
+	#  - +:cause+ - The exception to be used as a cause, and ensures that
+	#    that is not inferred from the arguments. May be +nil+ to ensure
+	#    that no cause is inferred
+	def initialize(*args, **options)
+
+		@uses_cause_message = false
+
+		cz = options[:cause]
+
+		if cz
+
+			options = options.reject { |k, v| k == :cause }
+
+			@has_implicit_message = args.empty?
+
+			super *args
+
+			warn 'unexpected implicit message' if @has_implicit_message && self.message != self.class.to_s
+
+			@cause = cz
+		else
+
+			cz_ix = args.index { |arg| ::Exception === arg }
+
+			if cz_ix
+
+				args = args.dup
+
+				cz = args.delete_at cz_ix
+
+				if args.empty?
+
+					if !(cz.message || '').empty? && cz.class.to_s != cz.message
+
+						@uses_cause_message = true
+
+						args = [ cz.message ]
+					end
+				end
+			else
+
+				cz = $!
+			end
+
+			@has_implicit_message = args.empty?
+
+			super *args
+
+			warn 'unexpected implicit message' if @has_implicit_message && self.message != self.class.to_s
+
+			@cause = cz
+		end
+
+		@options = options
+	end
+
+	# The cause / inner-exception, if any, specified to the instance
+	# initialiser
+	attr_reader :cause
+
+	# The options passed to the initialiser, with +:cause+ removed, if
+	# present
+	attr_reader :options
+
+	def chained_message **options
+
+		return message unless cause
+		return message if @uses_cause_message
+
+		m	=	message
+		c	=	cause
+		cm	=	c.respond_to?(:chained_message) ? c.chained_message(**options) : c.message
+
+		return m if (cm || '').empty?
+		return cm if (m || '').empty?
+
+		sep	=	options[:separator] || ': '
+
+		"#{m}#{sep}#{cm}"
+	end
+
+	# An array of exceptions in the chain, excluding +self+
+	def chainees
+
+		return [] unless cause
+
+		r = [ cause ]
+
+		r += cause.chainees if cause.respond_to? :chainees
+
+		r
+	end
+
+	# An array of exceptions in the chain, including +self+
+	def exceptions
+
+		[ self ] + chainees
+	end
+
+	# A combination of the backtrace(s) of all chained exception(s)
+	def chained_backtrace
+
+		b = backtrace
+
+		return b unless cause
+
+		cb = cause.respond_to?(:chained_backtrace) ? cause.chained_backtrace : cause.backtrace
+
+		(cb - b) + b
+	end
+end
+
+end # module Exceptions
+end # module Diagnostics
+end # module Xqsr3
---
+++ test/unit/diagnostics/exceptions/tc_with_cause.rb
@@ -0,0 +1,262 @@
+#! /usr/bin/env ruby
+
+$:.unshift File.join(File.dirname(__FILE__), '../../../../lib')
+
+require 'xqsr3/diagnostics/exceptions/with_cause'
+
+require 'test/unit'
+
+class Test_WithCause < Test::Unit::TestCase
+
+	class SomeExceptionWithCause < ::Exception
+
+		include ::Xqsr3::Diagnostics::Exceptions::WithCause
+	end
+
+	def test_no_ctor_args
+
+		x = SomeExceptionWithCause.new
+
+		assert_nil x.cause
+		assert_equal SomeExceptionWithCause.to_s, x.message
+		assert_equal SomeExceptionWithCause.to_s, x.chained_message
+		assert_empty x.chainees
+		assert_equal [ x ], x.exceptions
+		assert_nil x.backtrace
+		assert_empty x.options
+	end
+
+	def test_1_ctor_arg_that_is_a_message
+
+		msg = 'stuff'
+
+		x = SomeExceptionWithCause.new msg
+
+		assert_nil x.cause
+		assert_equal msg, x.message
+		assert_equal msg, x.chained_message
+		assert_empty x.chainees
+		assert_equal [ x ], x.exceptions
+		assert_nil x.backtrace
+		assert_empty x.options
+	end
+
+	def test_1_ctor_arg_that_is_a_cause_and_has_no_message
+
+		c = RuntimeError.new
+
+		x = SomeExceptionWithCause.new c
+
+		assert_not_nil x.cause
+		assert_equal SomeExceptionWithCause.to_s, x.message
+		assert_equal "#{SomeExceptionWithCause.to_s}: #{RuntimeError.to_s}", x.chained_message
+		assert_not_empty x.chainees
+		assert_equal [ c ], x.chainees
+		assert_equal [ x, c ], x.exceptions
+		assert_nil x.backtrace
+		assert_empty x.options
+	end
+
+	def test_1_ctor_arg_that_is_a_cause_and_has_a_message
+
+		c = RuntimeError.new 'blah'
+
+		x = SomeExceptionWithCause.new c
+
+		assert_not_nil x.cause
+		assert_equal 'blah', x.message
+		assert_equal 'blah', x.chained_message
+		assert_not_empty x.chainees
+		assert_equal [ c ], x.chainees
+		assert_equal [ x, c ], x.exceptions
+		assert_nil x.backtrace
+		assert_empty x.options
+	end
+
+	def test_2_ctor_args_that_are_message_and_cause
+
+		msg = 'stuff'
+
+		c = RuntimeError.new
+
+		x = SomeExceptionWithCause.new msg, c
+
+		assert_not_nil x.cause
+		assert_equal msg, x.message
+		assert_equal "#{msg}: #{RuntimeError.to_s}", x.chained_message
+		assert_not_empty x.chainees
+		assert_equal [ c ], x.chainees
+		assert_equal [ x, c ], x.exceptions
+		assert_nil x.backtrace
+		assert_empty x.options
+	end
+
+	def test_2_ctor_args_that_are_message_and_cause_that_has_a_message
+
+		msg = 'stuff'
+
+		c = RuntimeError.new 'blah'
+
+		x = SomeExceptionWithCause.new msg, c
+
+		assert_not_nil x.cause
+		assert_equal msg, x.message
+		assert_equal 'stuff: blah', x.chained_message
+
+		assert_not_empty x.chainees
+		assert_equal [ c ], x.chainees
+		assert_equal [ x, c ], x.exceptions
+		assert_nil x.backtrace
+		assert_empty x.options
+	end
+
+	def test_cause_in_options
+
+		c = RuntimeError.new 'inner'
+
+		x = SomeExceptionWithCause.new 'outer', cause: c
+
+		assert_not_nil x.cause
+		assert_equal 'outer', x.message
+		assert_equal 'outer: inner', x.chained_message
+
+		assert_not_empty x.chainees
+		assert_equal [ c ], x.chainees
+		assert_equal [ x, c ], x.exceptions
+		assert_nil x.backtrace
+		assert_empty x.options
+	end
+
+
+
+	class GrandchildException < Exception
+
+		include ::Xqsr3::Diagnostics::Exceptions::WithCause
+	end
+
+	class ChildException < Exception
+
+		def initialize(*args, **options)
+
+			super *args, **options
+		end
+
+		include ::Xqsr3::Diagnostics::Exceptions::WithCause
+	end
+
+	class ParentException < Exception
+
+		include ::Xqsr3::Diagnostics::Exceptions::WithCause
+
+		def initialize(*args, **options)
+
+			super *args, **options
+		end
+	end
+
+	class GrandparentException < Exception
+
+		include ::Xqsr3::Diagnostics::Exceptions::WithCause
+	end
+
+
+	def test_four_levels
+
+		gc = GrandchildException.new 'gc'
+
+		c = ChildException.new 'c', gc
+
+		p = ParentException.new c, 'p'
+
+		gp = GrandparentException.new 'gp', cause: p
+
+		assert_equal 'gp: p: c: gc', gp.chained_message
+		assert_equal 'gp-p-c-gc', gp.chained_message(separator: '-')
+	end
+end
+
+class Test_WithCause_throwing < Test::Unit::TestCase
+
+	class SomeExceptionWithCause < ::Exception
+
+		include ::Xqsr3::Diagnostics::Exceptions::WithCause
+	end
+
+	def f m
+
+		raise SomeExceptionWithCause, m
+	end
+
+	def g m, n
+
+		begin
+
+			f n
+		rescue Exception => x
+
+			raise SomeExceptionWithCause.new m, x
+		end
+	end
+
+	def h m, n, o
+
+		begin
+
+			g n, 0
+		rescue Exception => x
+
+			raise SomeExceptionWithCause.new m, x
+		end
+	end
+
+	def test_one_level
+
+		begin
+
+			f 'one-level'
+
+			assert false, 'should not get here!'
+		rescue Exception => x
+
+			assert_nil x.cause
+			assert_equal 'one-level', x.message
+			assert_equal 'one-level', x.chained_message
+			assert_empty x.chainees
+			assert_not_empty x.backtrace
+
+			x_bt0 = x.backtrace[0]
+
+			assert /:in\s+\`f\'\s*$/ =~ x_bt0, 'not receieved from f()'
+		end
+	end
+
+	def test_two_levels
+
+		begin
+
+			g 'two-levels', 'one-level'
+
+			assert false, 'should not get here!'
+		rescue Exception => x
+
+			assert_not_nil x.cause
+			assert_equal 'two-levels', x.message
+			assert_equal 'two-levels: one-level', x.chained_message
+			assert_not_empty x.chainees
+			assert_kind_of SomeExceptionWithCause, x.chainees[0]
+			assert_not_empty x.backtrace
+			assert_not_empty x.cause.backtrace
+
+			x_bt0 = x.backtrace[0]
+
+			assert /:in\s+\`rescue in g\'\s*$/ =~ x_bt0, 'not receieved from g()'
+
+			c_bt0 = x.cause.backtrace[0]
+
+			assert /:in\s+\`f\'\s*$/ =~ c_bt0, 'not receieved from f()'
+
+			assert_not_empty x.chained_backtrace
+		end
+	end
+end
+
