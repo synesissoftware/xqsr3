7ebc543c1a7adc0497700e5d100db22c16ba343f
Matt <stlsoft@gmail.com>
2018-09-05 08:53:53 +1000

    + added InspectBuilder

--- lib/xqsr3/version.rb
+++ lib/xqsr3/version.rb
@@ -50,7 +50,7 @@
 module Xqsr3
 
 	# Current version of the Xqsr3 library
-	VERSION				=	'0.27.3'
+	VERSION				=	'0.28.1'
 
 	private
 	VERSION_PARTS_		=	VERSION.split(/[.]/).collect { |n| n.to_i } # :nodoc:
