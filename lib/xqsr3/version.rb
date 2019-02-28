cbb7a57065bfc32a42e18586b6ebaf75d1931c07
Matt <matthew@synesis.com.au>
2019-02-28 17:45:18 +1100

    - removed XML components

--- lib/xqsr3/version.rb
+++ lib/xqsr3/version.rb
@@ -50,7 +50,7 @@
 module Xqsr3
 
 	# Current version of the Xqsr3 library
-	VERSION				=	'0.30.3'
+	VERSION				=	'0.31.0'
 
 	private
 	VERSION_PARTS_		=	VERSION.split(/[.]/).collect { |n| n.to_i } # :nodoc:
